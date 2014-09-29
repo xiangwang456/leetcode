2b6d422823043da36b793555994722bc7b04c166
hiyou <656453@qq.com>
2014-09-29 21:27:27 +0800

    ~ e111

---
+++ eclipse_workspace/RandomizedQueue/src/Subset.java
@@ -0,0 +1,20 @@
+import javax.swing.text.html.HTMLEditorKit.Parser;
+
+
+public class Subset {
+
+
+	public static void main(String[] args) {
+		int k=Integer.parseInt(args[0]);
+		
+		RandomizedQueue<String> rq=new RandomizedQueue<String>();
+		while(!StdIn.isEmpty())
+		{
+			rq.enqueue(StdIn.readString());
+		}
+		
+		for(int i=0;i<k;i++)
+			StdOut.println(rq.dequeue());
+	}
+
+}
---
+++ vs2012_workspace/jiudu/链表中倒数第k个结点/链表中倒数第k个结点.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	list<int>::size_type n;//listÔªËØ¸öÊý
+	while (scanf_s("%d", &n) != EOF)
+	{
+		int result;
+		list<int> ilist(n);
+		list<int>::size_type lastIndex; 
+		scanf_s("%d", &lastIndex);
+
+		list<int>::size_type t = n - lastIndex;
+		for(int i=0;i<n;i++)
+		{
+			int num;
+			scanf_s("%d", &num);
+			if (t == i)
+				result = num;
+			ilist.push_back(num);
+		}
+		if (t < 0||lastIndex==0)
+			printf("NULL\n");
+		else
+			printf("%d\n", result);
+	}
+}
