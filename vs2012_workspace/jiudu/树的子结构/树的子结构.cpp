2148e70b9d806ca520eefcb23b67c7c12c02478a
hiyou <656453@qq.com>
2014-10-05 11:23:45 +0800

    10.5 11:09 8-124

---
+++ vs2012_workspace/jiudu/树的子结构/树的子结构.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+template<typename T> struct Node {
+	T value;
+	Node *next;
+};
+
+struct Binarytree {
+	Node<int> value;
+	Binarytree *left;
+	Binarytree *right;
+};
+
+
+
+void main()
+{
+	int num_a;
+	int num_b;
+	while (scanf("%d %d",&num_a,&num_b)!=EOF)
+	{
+		
+	}
+}
