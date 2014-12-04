41e309099aea8da95bf9877f1c9a8e60cd209559
hiyou <656453@qq.com>
2014-12-04 19:40:52 +0800

    first python

---
+++ python_workspace/practice/ont.py
@@ -0,0 +1,20 @@
+#name=raw_input('Please enter your name: ')
+#print 'hello,',name
+#print 'i\'m \"ok\"'
+
+#print '''line1
+#line2
+#line3'''
+
+#print '''line
+#line2 line
+#22
+#line3'''
+
+print '''1122
+33'''
+
+sum=0
+for x in range(101):	
+	sum=sum+x
+print sum
