9919ab584e8a08de011935b94caefba7b12ec7fd
zhangqi <656453@qq.com>
2015-03-05 16:06:18 +0800

    level order from down to yop

---
+++ leetcode/python/Binary Tree Level Order Traversal II/Binary_Tree_Level_Order_Traversal_II.py
@@ -0,0 +1,33 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrderBottom(self, root):
+        if root is None:
+            return []
+        level = []
+        queue = []
+        res = []
+        queue.append(root)
+        queue.append(None)
+        while queue:
+            root = queue.pop(0)
+            if root :
+                level.append(root.val)
+                if root.left:
+                    queue.append(root.left)
+                if root.right:
+                    queue.append(root.right)
+            else:
+                res.append(level[:])
+                level = []
+                if queue:
+                    queue.append(None)
+        res.reverse()
+        return res
