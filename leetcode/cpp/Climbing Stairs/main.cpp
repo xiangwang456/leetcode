1a74dfc1e0afe489720517035050b6c2215ff3e0
zhangqi <656453@qq.com>
2015-06-01 15:32:34 +0800

    clibing statirs

---
+++ leetcode/cpp/Climbing Stairs/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2015年6月1日
+ *      Author: messi
+ */
+#include "Solution.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	Solution solu;
+	cout << solu.climbStairs(5) <<endl;
+	return 0;
+}
+
+
