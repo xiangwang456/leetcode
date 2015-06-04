9babcd970c7df0018c496a0d99d3e5a2e044132e
zhangqi <656453@qq.com>
2015-06-04 14:56:01 +0800

    length of the last word

---
+++ leetcode/cpp/Length of last word/Solution.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		int n = s.size() -1;
+		while (n >=0 && s[n] == ' ')
+			n--;
+		int res = 0;
+		for (; n >= 0 && s[n] != ' '; n--, res++);
+		return res;
+	}
+};
+
+int main()
+{
+	Solution solu;
+	cout << solu.lengthOfLastWord("");
+	int end;
+	cin >> end;
+}
