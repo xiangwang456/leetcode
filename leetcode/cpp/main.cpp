129f4a438b081dde9f27540637b764caea1c4a88
zz <656453@qq.com>
2017-09-26 00:53:32 +0800

    reverse bits single number

---
+++ leetcode/cpp/main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by zz on 2017/9/26.
+//
+
+
+#include <vector>
+#include "reverse_bits.cpp"
+#include <iostream>
+
+
+int main() {
+    Solution solu;
+
+    /*   vector<int> nums;
+       nums.push_back(1);
+       nums.push_back(2);
+       nums.push_back(1);
+       nums.push_back(3);
+       nums.push_back(2);*/
+
+    auto name = "nana say :";
+
+
+    std::cout << name << solu.reverse_bits(1);
+
+
+}
