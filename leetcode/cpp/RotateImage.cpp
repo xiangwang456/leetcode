30233a927da97cedf30670208d70612062f09bb0
zz <656453@qq.com>
2017-10-08 00:16:51 +0800

    fix bug

--- leetcode/cpp/RotateImage.cpp
+++ leetcode/cpp/RotateImage.cpp
@@ -12,8 +12,7 @@ public:
     void rotate(vector<vector<int>> &matrix) {
         int n = matrix.size();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - j; j++) {
-                // matrix[i][j] = matrix[n - 1 - i ][ n - 1 - j];
+            for (int j = 0; j < n - i; j++) {
                 swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
             }
         }
