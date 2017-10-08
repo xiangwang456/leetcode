56dcf039f7b66820fb897380133cc6de69f9b6b4
zz <656453@qq.com>
2017-10-09 00:45:17 +0800

    Best Time to Buy and Sell Stock

---
+++ leetcode/cpp/BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,29 @@
+//
+// Created by zz on 2017/10/9.
+// Time : O(n)
+// Space : O(1)
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+
+        if (prices.empty()) {
+            return 0;
+        }
+
+
+        int hold = numeric_limits<int>::min();
+        int release = numeric_limits<int>::min();
+        for (int price : prices) {
+            hold = max(-price, hold);
+            release = max(release, hold + price);
+        }
+
+        return release;
+    }
+};
+
