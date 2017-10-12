066b652c819fe805a3705653f772862585a0a25b
zz <656453@qq.com>
2017-10-13 00:00:28 +0800

    Summary Ranges

--- leetcode/cpp/SummaryRanges.cpp
+++ leetcode/cpp/SummaryRanges.cpp
@@ -16,7 +16,7 @@ public:
         if (nums.size() == 0) {
             return ranges;
         }
-        int start = nums[0], end = nums[1];
+        int start = nums[0], end = nums[0];
         for (int i = 1; i < nums.size(); i++) {
             if (i < nums.size() && nums[i] == end + 1) {
                 end++;
