60858a6aca516f5e32e2579451a005829786cf59
zz <656453@qq.com>
2017-10-15 00:36:51 +0800

    Product of Array Except Self

---
+++ leetcode/cpp/ProductofArrayExceptSelf.cpp
@@ -0,0 +1,34 @@
+//
+// Created by zz on 2017/10/15.
+// Time:  O(n)
+// Space: O(1)
+
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int> &nums) {
+        if (nums.empty()) return {};
+        vector<int> left_product(nums.size());
+        left_product[0] = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            left_product[i] = left_product[i - 1] * nums[i - 1];
+        }
+
+        int right_product = 1;
+
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            right_product *= nums[i + 1];
+            left_product[i] = left_product[i] * right_product;
+        }
+
+        return left_product;
+
+
+    }
+};
+
