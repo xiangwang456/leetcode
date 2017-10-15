5d271648994e35bab38d23fee96f53e53eb764ac
zz <656453@qq.com>
2017-10-16 00:40:10 +0800

    Search a 2D Matrix II

---
+++ leetcode/cpp/IncreasingTripletSubsequence.cpp
@@ -0,0 +1,28 @@
+//
+// Created by zz on 2017/10/16.
+
+// Time:  O(n)
+// Space: O(1)
+
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int> &nums) {
+        int min = numeric_limits<int>::max(), second_min = numeric_limits<int>::max();
+        for (auto &num : nums) {
+            if (num <= min) {
+                min = num;
+            } else if (num <= second_min) {
+                second_min = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
