65657831764686d8960b2a75881782cc80625025
zz <656453@qq.com>
2017-10-17 00:26:46 +0800

    Find All Duplicates in an Array

---
+++ leetcode/cpp/FindAllDuplicatesinanArray.cpp
@@ -0,0 +1,30 @@
+//
+// Created by zz on 2017/10/17.
+// Time : O(n)
+// Space : O(1)
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int> &nums) {
+        vector<int> ans;
+        for (int i = 0; i < nums.size();) {
+            if (nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            } else {
+                i++;
+            }
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != i + 1) {
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+
+    }
+};
