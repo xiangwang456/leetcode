6549470c47bf71e552da81a114bac83fa9e4de55
zz <656453@qq.com>
2017-10-17 21:32:41 +0800

    Beautiful Arrangement II

--- leetcode/cpp/BeautifulArrangementII.cpp
+++ leetcode/cpp/BeautifulArrangementII.cpp
@@ -1,4 +1,24 @@
 //
 // Created by zz on 2017/10/17.
-//
+// Time : O(n)
+// Space : O(1)
 
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> constructArray(int n, int k) {
+        int left = 1, right = n;
+        vector<int> ans;
+        while (left <= right) {
+            ans.emplace_back(k % 2 == 0 ? right-- : left++);
+            if (k > 1) {
+                k--;
+            }
+        }
+        return ans;
+    }
+};
