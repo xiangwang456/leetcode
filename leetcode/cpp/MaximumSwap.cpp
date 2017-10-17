c593efbceed28831cca7a56ee7a9e2491681cc20
zz <656453@qq.com>
2017-10-17 21:57:04 +0800

    Beautiful Arrangement II

---
+++ leetcode/cpp/MaximumSwap.cpp
@@ -0,0 +1,28 @@
+//
+// Created by zz on 2017/10/17.
+// Time : O(n)
+// Space: O(1)
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string digits = to_string(num);
+        int max_idx = digits.size() - 1;
+        int left = 0, right = 0;
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] > digits[max_idx]) {
+                max_idx = i;
+            } else if (digits[i] < digits[max_idx]) {
+                left = i;
+                right = max_idx;
+            }
+        }
+        swap(digits[left], digits[right]);
+        return stoi(digits);
+    }
+};
