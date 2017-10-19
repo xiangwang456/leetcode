ae46dcab5a73279a9bd527cbffb7ebf3e8c1a230
zhangqi <656453@qq.com>
2017-10-19 08:59:08 +0800

    test push

--- leetcode/cpp/ZigZagConversion.cpp
+++ leetcode/cpp/ZigZagConversion.cpp
@@ -16,14 +16,12 @@ public:
         }
         const int step = 2 * numRows - 2;
         string zigzag;
-        for (int i = 0; i < numRows; ++i) {
+        for (int i = 0; i < numRows; i++) {
             for (int j = i; j < s.length(); j += step) {
                 zigzag.push_back(s[j]);
                 if (0 < i && i < numRows - 1 &&
                     j + step - 2 * i < s.length()) {
                     zigzag.push_back(s[j + step - 2 * i]);
-
-
 
                 }
             }
