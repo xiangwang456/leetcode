548a835917d863c14f9b483126ba65bdee4daef3
zhangqi <656453@qq.com>
2017-10-23 17:59:40 +0800

    Ransom Note

---
+++ leetcode/cpp/RansomNote.cpp
@@ -0,0 +1,29 @@
+//
+// Created by zhangqi on 2017/10/23.
+// Time:  O(n)
+// Space: O(n)
+
+#include <string>
+#include <vector>
+
+
+using namespace std;
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> counts(26);
+        int letters = 0;
+        for(char ch : ransomNote){
+            if(counts[ch - 'a']++ == 0 ){
+                letters++;
+            }
+        }
+
+        for(char ch : magazine){
+            if(--counts[ch - 'a'] == 0 && --letters == 0 ){
+                return  true;
+            }
+        }
+        return  letters == 0;
+    }
+};
