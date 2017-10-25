c57786cf0096f673915bab56915e7eb13dad7c12
zz <656453@qq.com>
2017-10-25 23:19:32 +0800

    Reverse Words in a String III

---
+++ leetcode/cpp/ReverseWordsinaStringIII.cpp
@@ -0,0 +1,21 @@
+//
+// Created by zz on 2017/10/25.
+// Time:  O(n)
+// Space: O(1)
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        for (int i = 0, j = 0; j <= s.size(); j++) {
+            if (j == s.size() || s[j] == ' ') {
+                reverse(s.begin() + i, s.begin() + j);
+                i = j + 1;
+            }
+        }
+        return s;
+    }
+};
