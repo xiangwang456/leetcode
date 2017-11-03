f4201c558f630be031bb04095e547535b7b1505a
zhangqi <656453@qq.com>
2017-11-03 18:01:28 +0800

    Symmetric Tree

--- leetcode/cpp/main.cpp
+++ leetcode/cpp/main.cpp
@@ -9,11 +9,19 @@
 #include <string>
 #include <limits>
 #include <algorithm>
-
+#include "TreeNode.h"
+#include "SymmetricTree.cpp"
 
 using namespace std;
 
 int main() {
+    Solution solu ;
+    TreeNode node1 = TreeNode{0};
+    TreeNode node2 = TreeNode{1};
+    TreeNode node3 = TreeNode{1};
+    node1.left = &node2;
+    node1.right = &node2;
 
+    cout << solu.isSymmetric(&node1);
 
 }
