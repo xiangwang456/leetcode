aa17e3cf96614f2c89f81dc814408ca1a5a8c126
zhangqi <656453@qq.com>
2017-11-03 16:11:24 +0800

    Valid Parentheses

---
+++ leetcode/cpp/ValidParentheses.cpp
@@ -0,0 +1,33 @@
+//
+// Created by zhangqi on 2017/11/3.
+// Time:  O(n)
+// Space: O(n)
+
+#include <stack>
+#include <unordered_map>
+
+using  namespace std;
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        const  unordered_map<char, char> symbolPair = {{')', '('},
+                                                        {']', '['},
+                                                        {'}', '{'}};
+        stack<char> parentheses;
+        for(char ch: s) {
+            const auto &iter = symbolPair.find(ch);
+            if (iter != symbolPair.cend()) {
+                if (parentheses.empty() || parentheses.top() != iter->second) {
+                    return false;
+                }
+                parentheses.pop();
+            } else {
+                parentheses.push(ch);
+            }
+        }
+        return parentheses.empty();
+
+    }
+};
