d2b247c482803c2417f4c500bcbc1ac4a8545dee
zhangqi <656453@qq.com>
2017-11-14 18:08:48 +0800

    Kth Smallest Element in a Sorted Matrix

---
+++ leetcode/cpp/KthSmallestElementinaSortedMatrix.cpp
@@ -0,0 +1,29 @@
+//
+// Created by zhangqi on 2017/11/14.
+//
+
+#include <vector>
+#include <c++/regex>
+#include <c++/queue>
+
+using  namespace std;
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int ans = 0;
+        priority_queue<int, vector<int>, less<int>> maxHeap;
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
+                maxHeap.push(matrix[i][j]);
+                if(maxHeap.size() > k){
+                    maxHeap.pop();
+                }
+            }
+        }
+        if(!maxHeap.empty()){
+            ans = maxHeap.top();
+        }
+        return  ans;
+    }
+};
