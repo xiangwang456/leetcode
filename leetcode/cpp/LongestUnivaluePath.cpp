635db5c4b7828815fa04016e6a4a13f547ba5d95
zhangqi <656453@qq.com>
2017-11-17 17:33:33 +0800

    Longest Univalue Path :dfs

---
+++ leetcode/cpp/LongestUnivaluePath.cpp
@@ -0,0 +1,32 @@
+//
+// Created by zhangqi on 2017/11/17.
+//time : O(n)
+// space : O(h)
+
+#include "TreeNode.h"
+#include <functional>
+using namespace std;
+
+class Solution {
+public:
+    int longestUnivaluePath(TreeNode* root) {
+        int ans = 0;
+        dfs(root, ans);
+        return  ans;
+    }
+
+private:
+
+    int dfs(TreeNode *node, int &result){
+        if(node == nullptr){
+            return 0;
+        }
+
+        int left = dfs(node->left, result);
+        int right = dfs(node->right, result);
+        left = node->left != nullptr && node->left->val == node->val ? left + 1 : 0;
+        right = node->right != nullptr && node->right->val == node->val ? right + 1 : 0;
+        result = max( result, left + right);
+        return max(left, right);
+    }
+};
