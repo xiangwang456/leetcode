397a6719131d262d11e7d5a4799b53fd049f36c4
zhangqi <656453@qq.com>
2017-11-27 09:49:03 +0800

    LeetCode Number of Connected Components in an Undirected Graph

---
+++ leetcode/cpp/NumberofConnectedComponentsinanUndirectedGraph.cpp
@@ -0,0 +1,57 @@
+//
+// Created by zhangqi on 2017/11/27.
+
+//Time : O(n)
+//Space : O(n)
+
+
+#include <vector>
+#include <string>
+#include <c++/unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int countComponents(int n, vector<pair<int, int>>& edges) {
+        UnionFind uf(n);
+        for(const auto& e: edges ){
+            uf.unionSet(e.first, e.second);
+        }
+        return uf.length();
+    }
+
+private:
+
+    class UnionFind{
+    public:
+        UnionFind(const int n) : count_(0), set_(n, 0){
+
+        }
+
+        int findSet(const int x){
+            if(set_[x] != x){
+               set_[x] = findSet(x);
+            }
+            return set_[x];
+        }
+
+        void unionSet(const int x, const int y){
+            int xRoot = findSet(x), yRoot = findSet(y);
+            if(xRoot != yRoot){
+                set_[min(xRoot, yRoot)] = max(xRoot, yRoot);
+                count_--;
+            }
+        }
+
+        int length(){
+            return count_;
+        }
+
+
+    private:
+        int count_;
+        vector<int> set_;
+    };
+
+};
