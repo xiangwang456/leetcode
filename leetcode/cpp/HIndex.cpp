636f9360e9d1d16a7b79f9c5c4fc5b6b114ff3fa
zhangqi <656453@qq.com>
2017-12-19 15:00:59 +0800

    H Index

---
+++ leetcode/cpp/HIndex.cpp
@@ -0,0 +1,55 @@
+//
+// Created by zhangqi on 2017/12/19.
+
+#include <vector>
+#include <c++/functional>
+#include <c++/algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        vector<int> count(n + 1, 0);
+        for(const auto &citation : citations){
+            if(citation >= n){
+                count[n]++;
+            } else{
+                count[citation]++;
+            }
+        }
+
+        int h = 0;
+        for(int i = n; i >= 0; i--){
+            h += count[i];
+            if(h >= i){
+                return  i;
+            }
+        }
+
+        return  h;
+    }
+};
+
+
+
+//Time : O(nlogn)
+//Space: O(1)
+class Solution1 {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end(), greater<int>());
+
+        int ans = 0;
+        for(const auto &citation : citations){
+            if(citation >= ans + 1){
+                ans++;
+            } else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
