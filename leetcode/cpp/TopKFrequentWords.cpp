a0259757068401ea6dc0339322b304577a37e928
zhangqi <656453@qq.com>
2017-12-22 08:56:31 +0800

    Top K Frequent Words

---
+++ leetcode/cpp/TopKFrequentWords.cpp
@@ -0,0 +1,40 @@
+//
+// Created by zhangqi on 2017/12/21.
+//Time : O(nlog(n))
+//Space : O(n)
+
+
+#include <vector>
+#include <c++/unordered_map>
+#include <string>
+#include <c++/algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> counts;
+        for(const auto &word : words){
+            counts[word]++;
+        }
+
+        vector<vector<string>> buckets(words.size() + 1);
+        for(const auto &kvp : counts){
+            buckets[kvp.second].push_back(kvp.first);
+        }
+
+        vector<string> ans;
+        int index = 0;
+        for(int i = buckets.size() - 1; i >= 0; i--){
+            for(int j = 0 ; j < buckets[i].size(); j++){
+                sort(buckets[i].begin(), buckets[i].end());
+                if(index < k){
+                    ans.push_back(buckets[i][j]);
+                }
+            }
+        }
+        return ans;
+
+    }
+};
