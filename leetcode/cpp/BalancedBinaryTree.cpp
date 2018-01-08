10fc82ce828ab87cf722d556aee894d9b2024df0
zhangqi <656453@qq.com>
2018-01-08 09:12:36 +0800

    Balanced Binary Tree

---
+++ leetcode/cpp/BalancedBinaryTree.cpp
@@ -0,0 +1,34 @@
+//
+// Created by zhangqi on 2018/1/8.
+//Time : O(n)
+//Space : O(h)
+
+#include <c++/cmath>
+#include "TreeNode.h"
+
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(!root){
+            return  true;
+        }
+
+        int left = getHeight(root->left);
+        int right = getHeight(root->right);
+
+        return abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+    }
+
+private:
+    int getHeight(TreeNode *root){
+        if(!root){
+            return 0;
+        }
+
+        int left = getHeight(root->left);
+        int right = getHeight(root->right);
+
+        return left > right ? left + 1 : right + 1;
+    }
+};
