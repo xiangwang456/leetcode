2481a92cae19818045b3dc06f4947f8ce1a3ff03
zhangqi <656453@qq.com>
2018-01-29 10:07:59 +0800

    Search in Rotated Sorted Array　ＩＩ

---
+++ leetcode/cpp/SearchinRotatedSortedArrayII.cpp
@@ -0,0 +1,31 @@
+//
+// Created by zhangqi on 2018/1/29.
+// Time:  O(logn)
+// Space: O(1)
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int> &nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] == nums[left]) {
+                ++left;
+            } else if ((nums[mid] > nums[left] && nums[left] <= target && target < nums[mid]) ||  //左边有序
+                       (nums[mid] < nums[left] && !(nums[mid] < target && target <= nums[right]))) {  // 同样，如果nums[mid] < nums[left] 说明左边有序，右边乱序且值在左边
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+};
