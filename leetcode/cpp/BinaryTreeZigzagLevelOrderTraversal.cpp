e9b55d447af3cbf02e8a1ef683c70d45ee4ca025
zhangqi <656453@qq.com>
2018-01-31 09:53:24 +0800

    Binary Tree Level Order Traversal

--- leetcode/cpp/BinaryTreeZigzagLevelOrderTraversal.cpp
+++ leetcode/cpp/BinaryTreeZigzagLevelOrderTraversal.cpp
@@ -1,43 +1,45 @@
 //
 // Created by zhangqi on 2018/1/31.
+
 // Time:  O(n)
 // Space: O(n)
 
 #include <vector>
 #include <queue>
+#include <algorithm>
 #include "TreeNode.h"
 
 using namespace std;
 
+
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> result;
-        queue<TreeNode *> que;
-        if(root != nullptr){
+        queue<TreeNode*> que;
+        int leftToRight = 0;
+        if(root){
             que.emplace(root);
         }
         while (!que.empty()){
             vector<int> level;
             int size = que.size();
-            for(int i = 0; i < size; i++){
+            for(int i = 0 ; i < size; i++){
                 TreeNode *front = que.front();
-                level.push_back(front->val);
                 que.pop();
-
+                level.push_back(front->val);
                 if(front->left){
                     que.emplace(front->left);
                 }
-
                 if(front->right){
                     que.emplace(front->right);
                 }
             }
+            if(leftToRight++ % 2 == 1){
+                reverse(level.begin(), level.end());
+            }
             result.push_back(level);
         }
-
-
         return result;
     }
 };
