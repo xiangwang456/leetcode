ebf98ce31ccf20300ccbab5257452805f7d92910
zhangqi <656453@qq.com>
2018-02-02 11:04:57 +0800

    Word Ladder

--- leetcode/cpp/WordLadder.cpp
+++ leetcode/cpp/WordLadder.cpp
@@ -5,41 +5,55 @@
 
 #include <string>
 #include <vector>
-#include <c++/queue>
+#include <queue>
 #include <c++/unordered_set>
 
 using namespace std;
 
+
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+
+        if(beginWord == endWord){
+            return 1;
+        }
+
+        int wordSize = beginWord.size();
+        if(wordSize != endWord.size()){
+            return 0;
+        }
+
         queue<string> visited;
-        int result = 1;
+        int result = 2;
         visited.push(beginWord);
         unordered_set<string> dict;
         for (int i = 0; i < wordList.size(); i++) {
             dict.insert(wordList[i]);
         }
         while (!visited.empty()){
-            const string & word = visited.front();
-            visited.pop();
-            for(int i = 0; i < word.size(); i++){
-                char originChar = word[i];
-                for(char c = 'a'; c <= 'z'; c++){
-                    if(c == originChar){
-                        continue;
-                    }
-                    word[i] = c;
-
-                    if(dict.find(word) != dict.end()){
-                        if(word == endWord) {
-                            return result;
+            int queueSize = visited.size();
+            for(int i = 0; i < queueSize; i++){
+                string  word = visited.front();
+                visited.pop();
+                for(int j = 0; j < word.size(); j++){
+                    char originChar = word[j];
+                    for(char c = 'a'; c <= 'z'; c++){
+                        if(c == originChar){
+                            continue;
                         }
-                        visited.push(word);
-                        dict.erase(word);
+                        word[j] = c;
+
+                        if(dict.find(word) != dict.end()){
+                            if(word == endWord) {
+                                return result;
+                            }
+                            visited.push(word);
+                            dict.erase(word);
+                        }
                     }
+                    word[j] = originChar;
                 }
-                word[i] = originChar;
             }
             result++;
         }
